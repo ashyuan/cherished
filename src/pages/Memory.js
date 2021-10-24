2cdd8e99372c814d97ed8d7e776ad1474eaa9119
mirandachan <76542515+mcctm@users.noreply.github.com>
2021-10-24 16:04:28 -0700

    styling for memory page

--- src/pages/Memory.js
+++ src/pages/Memory.js
@@ -3,6 +3,7 @@ import { useNav } from '../customHooks/useNav';
 import UploadWidget from "./memory-components/UploadWidget"
 import DisplayImages from "./memory-components/DisplayImages"
 import './Page.css';
+import memories from '../pics/memories.jpg'
 
 
 const Memory = () => {
@@ -16,10 +17,11 @@ const Memory = () => {
 
 	return (
 		<section ref={memoryRef} id='memoryContainer' className="memory-section">
+			<img src= {memories} alt='a photo wall with pictures of different family members' />
 			<div className="memory-container">
-				<h3>MEMORIES</h3>
+				<h3>Memories and people I cherish? <i class="fas fa-camera-retro"></i> </h3>
 				<div className="upload-container"> 
-					<p>Upload your memories</p>
+					<p>Upload your memories, or people you want to remember, or both</p>
 					<div className="upload-widget"> <UploadWidget /> </div>
 					<div className="display"> <DisplayImages /> </div>
 				</div>
