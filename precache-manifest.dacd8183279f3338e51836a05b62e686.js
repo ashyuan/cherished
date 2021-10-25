b8e1ff7f83a4a15d2686c2b888d3e10f5953ac05
ashyuan <ashleyblossomyuan@gmail.com>
2021-10-24 18:29:05 -0700

    Updates

---
+++ precache-manifest.dacd8183279f3338e51836a05b62e686.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d40eb49b0284cd26025eb9b5056edfc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "406c8f232cf8da4dcfb9",
+    "url": "/static/css/main.13dd0fe7.chunk.css"
+  },
+  {
+    "revision": "d43da477b9b81402845c",
+    "url": "/static/js/2.4d877f7e.chunk.js"
+  },
+  {
+    "revision": "42c9291f686c148c36a1f8f511dd8e2a",
+    "url": "/static/js/2.4d877f7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "406c8f232cf8da4dcfb9",
+    "url": "/static/js/main.fd5b4e7f.chunk.js"
+  },
+  {
+    "revision": "b97ff5d95b0ca6d2b249",
+    "url": "/static/js/runtime-main.eb431ce1.js"
+  },
+  {
+    "revision": "aea3c34d2670aafe4084c68693ee5db0",
+    "url": "/static/media/about.aea3c34d.jpg"
+  },
+  {
+    "revision": "5e1b0ac071fdc4f5fbc9e3cc475024a7",
+    "url": "/static/media/girl-icon.5e1b0ac0.jpg"
+  },
+  {
+    "revision": "86b23e9693452dbcba8a47dd8086acf5",
+    "url": "/static/media/memories.86b23e96.jpg"
+  },
+  {
+    "revision": "b4ef92e5911b69574104cd5dbb046281",
+    "url": "/static/media/pic1.b4ef92e5.jpeg"
+  },
+  {
+    "revision": "9a1d8204a2ca628ae1dff76145efac51",
+    "url": "/static/media/pic2.9a1d8204.jpeg"
+  },
+  {
+    "revision": "9aca542b20343b195d75b0aaba2ae788",
+    "url": "/static/media/senior.9aca542b.jpg"
+  }
+]);
